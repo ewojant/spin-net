59decb41133f346eec755c28937be68b3365133a
wojanton <wojciech.antosiewicz@gmail.com>
2018-06-30 18:21:21 +0200

    spinet.app cleanup
    Removed spinet_app module

--- src/spinet.erl
+++ src/spinet.erl
@@ -1,11 +1,26 @@
 %% @author wojanton
 
 -module(spinet).
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+-export([degree/4]).
+
+start(_StartType, StartArgs) ->
+    ok = logger:add_handler(my_standard_h, logger_std_h,
+                #{level => info, filter_default => log,
+                  logger_std_h =>
+                  #{type => {file, "./system_info.log"},
+                    filesync_repeat_interval => 1000}}),
+    logger:info("Starting spinet, Args=~p", [StartArgs]),
+    spinet_sup:start_link(StartArgs).
+
+stop(_State) -> ok.
 
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([degree/4]).
 
 degree(_N, _M, _Type, _Number) ->
     ok.
